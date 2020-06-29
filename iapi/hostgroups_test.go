4590554a487436f93e0e3de6e015e2c248043241
Hugo Soto <hugo@preyhq.com>
2020-06-29 19:21:14 -0400

    hostgroups: apply usage of subtesting

--- iapi/hostgroups_test.go
+++ iapi/hostgroups_test.go
@@ -4,70 +4,61 @@ import (
 	"testing"
 )
 
-func TestGetValidHostgroup(t *testing.T) {
+func TestHostgroups(t *testing.T) {
+	icingaServer := Server{"root", ICINGA2_API_PASSWORD, "https://127.0.0.1:5665/v1", true, nil}
+	t.Run("Create", func(t *testing.T) {
+		t.Run("Hostgroup", func(t *testing.T) {
+			name := "docker-servers"
+			displayName := "Docker Host Servers"
+			_, err := icingaServer.CreateHostgroup(name, displayName)
+			if err != nil {
+				t.Error(err)
+			}
+		})
+	})
 
-	name := "linux-servers"
+	t.Run("Read", func(t *testing.T) {
+		t.Run("ValidHostgroup", func(t *testing.T) {
+			name := "linux-servers"
+			_, err := icingaServer.GetHostgroup(name)
+			if err != nil {
+				t.Error(err)
+			}
+		})
 
-	_, err := Icinga2_Server.GetHostgroup(name)
+		t.Run("InvalidHostgroup", func(t *testing.T) {
+			name := "irix-servers"
+			_, err := icingaServer.GetHostgroup(name)
+			if err != nil {
+				t.Error(err)
+			}
+		})
+	})
 
-	if err != nil {
-		t.Error(err)
-	}
+	t.Run("Delete", func(t *testing.T) {
+		// Delete Hostgroup created via API. Should succeed
+		t.Run("Hostgroup", func(t *testing.T) {
+			name := "docker-servers"
+			err := icingaServer.DeleteHostgroup(name)
+			if err != nil {
+				t.Error(err)
+			}
+		})
 
-}
+		t.Run("HostgroupNonAPI", func(t *testing.T) {
+			name := "linux-servers"
+			err := icingaServer.DeleteHostgroup(name)
+			if err.Error() != "500 One or more objects could not be deleted" {
+				t.Error(err)
+			}
+		})
 
-func TestGetInvalidHostgroup(t *testing.T) {
-
-	name := "irix-servers"
-
-	_, err := Icinga2_Server.GetHostgroup(name)
-	if err != nil {
-		t.Error(err)
-	}
-
-}
-
-func TestCreateHostgroup(t *testing.T) {
-
-	name := "docker-servers"
-	displayName := "Docker Host Servers"
-	_, err := Icinga2_Server.CreateHostgroup(name, displayName)
-
-	if err != nil {
-		t.Error(err)
-	}
-
-}
-
-// func TestDeleteHostgroup
-// Delete Hostgroup created via API. Should succeed
-func TestDeleteHostgroup(t *testing.T) {
-
-	name := "docker-servers"
-
-	err := Icinga2_Server.DeleteHostgroup(name)
-	if err != nil {
-		t.Error(err)
-	}
-}
-
-// func TestDeleteHostgroupNonAPI
-func TestDeleteHostgroupNonAPI(t *testing.T) {
-
-	name := "linux-servers"
-
-	err := Icinga2_Server.DeleteHostgroup(name)
-	if err.Error() != "500 One or more objects could not be deleted" {
-		t.Error(err)
-	}
-}
-
-func TestDeleteHostgroupDNE(t *testing.T) {
-
-	name := "docker-servers"
-	err := Icinga2_Server.DeleteHostgroup(name)
-
-	if err.Error() != "No objects found." {
-		t.Error(err)
-	}
+		t.Run("HostgroupDNE", func(t *testing.T) {
+			name := "docker-servers"
+			err := icingaServer.DeleteHostgroup(name)
+			if err.Error() != "No objects found." {
+				t.Error(err)
+			}
+		})
+	})
 }
