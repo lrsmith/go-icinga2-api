0bf531132b1a21f356366580c7237246d16467c5
Hugo Soto <hugo@preyhq.com>
2020-06-29 18:33:21 -0400

    services: use env var for icinga password

--- iapi/services_test.go
+++ iapi/services_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func TestServices(t *testing.T) {
-	icingaServer := Server{"root", "", "https://127.0.0.1:5665/v1", true, nil}
+	icingaServer := Server{"root", ICINGA2_API_PASSWORD, "https://127.0.0.1:5665/v1", true, nil}
 	testHostName := "c1-mysql-1"
 	_, err := icingaServer.CreateHost(testHostName, "127.0.0.1", "hostalive", nil, nil, nil)
 	if err != nil {
